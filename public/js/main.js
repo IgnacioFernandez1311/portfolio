5de4043ec3b1db9c37e77687c47eca23d646c43f
Ignacio Fernandez <chonp64@gmail.com>
2022-11-28 21:16:16 -0300

    feature - CSS styles

--- public/js/main.js
+++ public/js/main.js
@@ -3,10 +3,7 @@ var hamburgerMenu = document.querySelector(".navbar__menu")
 var navbarList = document.querySelector(".navbar__ul")
 var navbarItem = document.querySelector(".navbar__link")
 
+
 hamburgerMenu.onclick = function() {
     navbarList.classList.toggle("navbar__ul--active")
-}
-
-navbarItem.onclick = function() {
-    navbarList.classList.remove("navbar__ul--active")
 }
