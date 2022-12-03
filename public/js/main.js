7ddc1f725bdd1939597a8e0ea26ad924245034a8
Ignacio Fernandez <chonp64@gmail.com>
2022-12-03 12:38:49 -0300

    feature - card flip, menu

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,29 @@
+addEventListener("DOMContentLoaded", () => {
+    function removeNavbarList() {
+        navbarList.classList.remove("navbar__ul--active")
+    }
 
-var hamburgerMenu = document.querySelector(".navbar__menu")
-var navbarList = document.querySelector(".navbar__ul")
-var navbarItem = document.querySelector(".navbar__link")
+    const hamburgerMenu = document.querySelector(".navbar__menu")
+    const navbarList = document.querySelector(".navbar__ul")
+    const navbarItems = document.querySelectorAll(".navbar__link")
+    const navbarMenuClose = document.querySelector(".navbar__menu_close")
+    const projectItemsFlip = document.querySelectorAll(".main__project_item")
+
+    hamburgerMenu.onclick = function () {
+        navbarList.classList.add("navbar__ul--active")
+    }
+
+    navbarMenuClose.onclick = function () {
+        removeNavbarList()
+    }
+
+    navbarItems.forEach(item => item.addEventListener("click", () => {
+        removeNavbarList()
+    }))
+
+    projectItemsFlip.forEach(item => item.addEventListener("click", () => {
+        item.classList.toggle("rotated")
+    }))
+})
 
 
-hamburgerMenu.onclick = function() {
-    navbarList.classList.toggle("navbar__ul--active")
-}
