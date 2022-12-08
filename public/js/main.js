8efd7657a4cf61d49fe12e620fd11983a4631c49
Ignacio Fernandez <chonp64@gmail.com>
2022-12-07 23:17:41 -0300

    final fixes

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,30 @@
+addEventListener("DOMContentLoaded", () => {
+    function removeNavbarList() {
+        navbarList.classList.remove("navbar__ul--active")
+    }
+    
+    const hamburgerMenu = document.querySelector(".navbar__menu")
+    const navbarList = document.querySelector(".navbar__ul")
+    const navbarItems = document.querySelectorAll(".navbar__link")
+    const navbarMenuClose = document.querySelector(".navbar__menu_close")
+    const projectItemsFlip = document.querySelectorAll(".main__project_item")
+    
+    hamburgerMenu.onclick = function () {
+        navbarList.classList.add("navbar__ul--active")
+    }
+    
+    navbarMenuClose.onclick = function () {
+        removeNavbarList()
+    }
+    
+    navbarItems.forEach(item => item.addEventListener("click", () => {
+        removeNavbarList()
+    }))
+    
+    projectItemsFlip.forEach(item => item.addEventListener("click", () => {
+        item.classList.toggle("rotated")
+    }))
+})
 
-function removeNavbarList() {
-    navbarList.classList.remove("navbar__ul--active")
-}
-
-const hamburgerMenu = document.querySelector(".navbar__menu")
-const navbarList = document.querySelector(".navbar__ul")
-const navbarItems = document.querySelectorAll(".navbar__link")
-const navbarMenuClose = document.querySelector(".navbar__menu_close")
-const projectItemsFlip = document.querySelectorAll(".main__project_item")
-
-hamburgerMenu.onclick = function () {
-    navbarList.classList.add("navbar__ul--active")
-}
-
-navbarMenuClose.onclick = function () {
-    removeNavbarList()
-}
-
-navbarItems.forEach(item => item.addEventListener("click", () => {
-    removeNavbarList()
-}))
-
-projectItemsFlip.forEach(item => item.addEventListener("click", () => {
-    item.classList.toggle("rotated")
-}))
 
 
